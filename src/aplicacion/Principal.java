dd6ccfc318031fdf976ee8c135da125087cdde65
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 17:02:12 +0100

    ejercicio terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.ej2_2_lambda;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int n = sc.nextInt();
+        System.out.println("El factorial de " + n + " es: " + ej2_2_lambda.factorial(n));
+    }
 }
--- src/dominio/ej2_2_lambda.java
+++ src/dominio/ej2_2_lambda.java
@@ -1,2 +1,8 @@
-package dominio;public class ej2_2_lambda {
+package dominio;
+import java.util.stream.Stream;
+
+public class ej2_2_lambda {
+    public static int factorial(int n){
+        return Stream.iterate(1,i->i+1).limit(n).reduce(1,(a,b)->a*b);
+    }
 }
